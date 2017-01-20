95ee1942049bbebe4f47d57a0863ae2eedbfcc6e
Allison Thongkham <aft5118@psu.edu>
2017-01-19 20:58:09 -0500

    Create DrawingApplication.java

---
+++ DrawingApplication.java
@@ -0,0 +1,11 @@
+package DrawingApplication;
+
+import javax.swing.JFrame;
+
+   public class DrawingApplication {
+    public static void main( String args[] )
+    {
+        DrawFrame paintGui = new DrawFrame();     
+        paintGui.setSize(800,600);
+    }
+} 
